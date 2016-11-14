19c496e680903ff779cc4cc08c7c766c4a2aa545
mshr-h <mshr-h@users.noreply.github.com>
2016-11-14 17:05:20 +0900

    update processor element

--- rtl/pe.v
+++ rtl/pe.v
@@ -18,14 +18,24 @@ always @(posedge clk or negedge rst_n) begin
     nxt_tb <= 0;
   end else begin
     if(en_sw)
-      nxt_sw <= pel_sw;
+      nxt_sw <= #1 pel_sw;
     if(en_tb)
-      nxt_tb <= pel_tb;
+      nxt_tb <= #1 pel_tb;
   end
 end
 
-assign ad = (nxt_sw > nxt_tb) ? nxt_sw - nxt_tb
-                              : nxt_tb - nxt_sw;
+// 8-bit AD
+// assign ad = (nxt_sw > nxt_tb) ? nxt_sw - nxt_tb
+                              // : nxt_tb - nxt_sw;
+
+// 4-bit AD
+// assign ad[3:0] = (nxt_sw[7:4] > nxt_tb[7:4]) ? nxt_sw[7:4] - nxt_tb[7:4]
+                                             // : nxt_tb[7:4] - nxt_sw[7:4];
+
+// 2-bit AD, 6-bit XOR
+// assign ad[7:6] = (nxt_sw[7:6] > nxt_tb[7:6]) ? nxt_sw[7:6] - nxt_tb[7:6]
+                                             // : nxt_tb[7:6] - nxt_sw[7:6];
+// assign ad[5:0] = nxt_sw[5:0] ^ nxt_tb[5:0];
 
 endmodule
 `default_nettype wire
